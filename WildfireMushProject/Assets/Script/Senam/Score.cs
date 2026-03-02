70d50d2d87831ae804f7bba87a7214bff1e2b689
bm1688 <bmwachirawit88@gmail.com>
2026-03-02 10:19:50 +0700

    Assemble all element in sample scene

--- WildfireMushProject/Assets/Script/Senam/Score.cs
+++ WildfireMushProject/Assets/Script/Senam/Score.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Score : MonoBehaviour
 {
     // when touch this script item will tell the score manager to add score by 1
-    private void OnTriggerEnter(Collider other)
+
+    private void OnTriggerEnter2D  (Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
