55b896df6bb5919f6e0845f9752b1246fab3fce0
Mong-Ying1150 <napapan.sawa@gmail.com>
2026-03-04 22:08:55 +0700

    Oxygen Bar
    Hold spacebar to hold breath

--- WildfireMushProject/Assets/Script/Alex/OxygenBar.cs
+++ WildfireMushProject/Assets/Script/Alex/OxygenBar.cs
@@ -1,18 +1,20 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OxygenBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Slider slider;
 
-    // Update is called once per frame
-    void Update()
+    public void SetMaxHealth(float health)
     {
-        
+        slider.maxValue = health;
+        slider.value = health;
+    }
+    public void SetHealth(float health)
+    {
+        slider.value = health;
     }
 }
--- WildfireMushProject/Assets/Script/Alex/PlayerHealth.cs
+++ WildfireMushProject/Assets/Script/Alex/PlayerHealth.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+
+    public float currentHealth = 50;
+    public float maxHealth = 100;
+    public OxygenBar oxygen;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        oxygen.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            TakeDamage(10f * Time.deltaTime);
+        }
+        else
+        {
+            TakeDamage(-27f  * Time.deltaTime);
+        }
+
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+
+        oxygen.SetHealth(currentHealth);
+    }
+    void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
     }
 }
