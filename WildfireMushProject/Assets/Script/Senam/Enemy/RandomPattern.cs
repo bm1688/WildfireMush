c3410cf623335de26775b71bb7f9d6eca35c8b47
RiddleGallery <senum.sume@gmail.com>
2026-03-08 20:25:41 +0700

    enemy random pattern

---
+++ WildfireMushProject/Assets/Script/Senam/Enemy/RandomPattern.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPattern : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private Vector3 position;
+    [SerializeField] private Vector3 velocity;
+    private Rigidbody _rb;
+    private Collider2D _collider;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _collider = GetComponent<Collider2D>();
+        position = transform.position;
+        velocity = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0).normalized * speed;
+    }
+    private void Update()
+    {
+        position += velocity * Time.deltaTime;
+        transform.position = position;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Wall>())
+        {
+            velocity = Vector3.Reflect(velocity, collision.contacts[0].normal);
+            velocity += new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), 0);
+        }
+        else if (collision.gameObject.GetComponent<PlayerMovement>())
+        {
+            Debug.Log("Game Over");
+            Time.timeScale = 0f;
+        }
+    }
+}
---
+++ WildfireMushProject/Assets/Script/Senam/Wall.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+// This script is for the wall as a ref
+}
