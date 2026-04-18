4a39ba205d14e3ded68b02811ed5a39d5f4e1e5a
Phumiphat <122847294+Phum-digitaltechnology@users.noreply.github.com>
2026-04-19 00:52:38 +0700

    AudioManager

---
+++ WildfireMushProject/Assets/Script/Phum/AudioManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Unity.VisualScripting;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    public Sound[] musicSounds, sfxSounds;
+    public AudioSource musicSource, sfxSource;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    private void Start()
+    {
+        //PlayMusic("background theme song");
+    }
+
+    public void PlayMusic(string name)
+    {
+        Sound s = Array.Find(musicSounds, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.Log("Sound not found");
+        }
+        else
+        {
+            musicSource.clip = s.clip;
+            musicSource.Play();
+        }
+    }
+    public void PlaySFX(string name)
+    {
+        Sound s = Array.Find(sfxSounds, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.Log("Sound not found");
+        }
+        else
+        {
+
+            sfxSource.PlayOneShot(s.clip, s.volume);
+        }
+    }
+
+
+}
