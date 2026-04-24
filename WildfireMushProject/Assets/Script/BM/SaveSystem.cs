fd7bc0a77ad8bcde164f61e44325ff4af2d17da7
bm1688 <bmwachirawit88@gmail.com>
2026-04-24 15:08:21 +0700

    updateAimandShoot and SaveSystem
    add drainrate to Aim and Shoot
    update savesystem

--- WildfireMushProject/Assets/Script/BM/SaveSystem.cs
+++ WildfireMushProject/Assets/Script/BM/SaveSystem.cs
@@ -8,15 +8,25 @@ public class SaveSystem : MonoBehaviour
     [SerializeField] private O2TankSO _o2TankSO;
     [SerializeField] private FuelTankSO _fuelTankSO;
     [SerializeField] private ShoeSO _shoeSO;
+
+    [SerializeField] private string O2DATA;
+    [SerializeField] private string FuelTankDATA;
+    [SerializeField] private string ShoeDATA;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(_loadoutManager.GetCurrentLoadout());
+        PlayerPrefs.GetInt(O2DATA);
+        PlayerPrefs.GetInt(FuelTankDATA);
+        PlayerPrefs.GetInt(ShoeDATA);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerPrefs.SetInt(O2DATA, 0);
+        Debug.Log(PlayerPrefs.GetInt(O2DATA));
+        PlayerPrefs.SetInt(O2DATA, 1);
+        Debug.Log(PlayerPrefs.GetInt(O2DATA));
     }
 }
