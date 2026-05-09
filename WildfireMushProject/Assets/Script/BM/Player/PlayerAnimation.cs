16c83706ca6495238897949a1070f72a83e7e98c
bm1688 <bmwachirawit88@gmail.com>
2026-05-09 14:14:28 +0700

    Fixed Player animation

--- WildfireMushProject/Assets/Script/BM/Player/PlayerAnimation.cs
+++ WildfireMushProject/Assets/Script/BM/Player/PlayerAnimation.cs
@@ -28,7 +28,7 @@ public class PlayerAnimation : MonoBehaviour
             _spriteRenderer.flipX = true;
         }
 
-        if (!_playerAimAndShoot.Shooting)
+        //if (!_playerAimAndShoot.Shooting)
         {
             if (_playerMovement.MoveX > 0)
             {
@@ -69,6 +69,7 @@ public class PlayerAnimation : MonoBehaviour
             else
             {
                 _playerAnimator.SetBool("IsShooting", false);
+                _playerAnimator.SetBool("IsRunning", false );
             }
         }
 
