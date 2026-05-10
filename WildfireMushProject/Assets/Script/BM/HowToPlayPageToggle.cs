cd036f0b48c5852badbc728591108fcca6210e22
bm1688 <bmwachirawit88@gmail.com>
2026-05-10 14:16:25 +0700

    Update how to play page toggle

--- WildfireMushProject/Assets/Script/BM/HowToPlayPageToggle.cs
+++ WildfireMushProject/Assets/Script/BM/HowToPlayPageToggle.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class HowToPlayPageToggle : MonoBehaviour
 {
+    [SerializeField] private List<GameObject> _howToPlayPanel = new List<GameObject>();
+
+    [SerializeField] private int _toggleValue;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,35 @@ public class HowToPlayPageToggle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_toggleValue == 0)
+            {
+                Toggle();
+                
+            }
+            else if (_toggleValue == 1)
+            {
+                Toggle();
+            }
+            else if (_toggleValue == 2)
+            {
+                Toggle();
+            }
+            else
+            {
+                Toggle();
+                _toggleValue = 0;
+            }
+        }
+
         
+    }
+    private void Toggle()
+    {
+        _howToPlayPanel[_toggleValue].SetActive(true);
+        _toggleValue += 1;
+        if (_toggleValue == 0) { return; }
+        _howToPlayPanel[_toggleValue - 1].SetActive(false);
     }
 }
