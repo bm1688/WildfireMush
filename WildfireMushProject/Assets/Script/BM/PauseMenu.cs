f5c96f9c6fd432b40121ade136597e04a5f0e43b
bm1688 <bmwachirawit88@gmail.com>
2026-05-10 15:40:35 +0700

    fix pause menu

--- WildfireMushProject/Assets/Script/BM/PauseMenu.cs
+++ WildfireMushProject/Assets/Script/BM/PauseMenu.cs
@@ -9,53 +9,59 @@ public class PauseMenu : MonoBehaviour
     [SerializeField] private Image _pausePanel;
 
     [SerializeField] private GameObject _loadoutUI;
-    [SerializeField] private GameObject _loadoutRootUI;
-    [SerializeField] private GameObject _loadoutSaveLoadUI;
+
+    [SerializeField] private GameObject _pauseUI;
 
     [SerializeField] private int _saveLoadToggleValue;
 
     [SerializeField] private int _toggleValue;
 
+    [SerializeField] private bool _loadoutActive = false;
+
+    [SerializeField] private bool _pauseActive = false;
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) && _toggleValue == 0)
+        if (Input.GetKeyDown(KeyCode.Escape) && _toggleValue == 0 && !_loadoutActive)
         {
             PauseGame();
         }
         else if (Input.GetKeyDown(KeyCode.Escape) && _toggleValue == 1)
         {
             Continue();
+            
+            
         }
 
-        if (Input.GetKeyDown(KeyCode.F) && _saveLoadToggleValue == 0)
+        if (Input.GetKeyDown(KeyCode.F) && _saveLoadToggleValue == 0 && !_pauseActive)
         {
             OpenLoadout();
         }
         else if (Input.GetKeyDown(KeyCode.F) && _saveLoadToggleValue == 1)
         {
             CloseLoadout();
+            
         }
     }
 
-    public void Resume()
-    {
-        Continue();
-    }
+    
 
     private void PauseGame()
     {
         _pausePanel.enabled = true;
-        //_loadoutUI.SetActive(true);
+        _pauseUI.SetActive(true);
+        _pauseActive = true;
         _toggleValue = 1;
         Time.timeScale = 0f;
     }
 
     private void Continue()
     {
-        CloseLoadout();
+        
         _pausePanel.enabled = false;
-        _loadoutUI.SetActive(false);
+        _pauseUI.SetActive(false);
+        _pauseActive = false;
         _toggleValue = 0;
         Time.timeScale = 1f;
     }
@@ -63,13 +69,15 @@ public class PauseMenu : MonoBehaviour
     private void OpenLoadout()
     {
         _loadoutUI.SetActive(true);
+        _loadoutActive = true;
         _saveLoadToggleValue = 1;
         Time.timeScale = 0f;
     }
         
     private void CloseLoadout()
     {
-        _loadoutUI.SetActive(false );
+        _loadoutUI.SetActive(false);
+        _loadoutActive = false;
         _saveLoadToggleValue = 0;
         Time.timeScale = 1f;
     }
